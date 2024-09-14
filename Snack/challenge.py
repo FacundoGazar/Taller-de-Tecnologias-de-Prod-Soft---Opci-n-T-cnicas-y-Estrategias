3dac9cc5da0b8e08f60d84b9ae2900316708a33f
Facundo <facundobarciabarate@gmail.com>
2024-09-14 04:03:56 -0300

    Snack

---
+++ Snack/challenge.py
@@ -0,0 +1,16 @@
+import sys
+
+# Leer inputs
+inp_str = sys.stdin.readline()
+inp_list = list(map(int, inp_str.rstrip().split()))
+
+X = inp_list[0]
+Y = inp_list[1]
+
+# Tabla de los precios pasado a lista
+table = [4, 4.5, 5, 2, 1.5]
+
+# Calculo el total segun la lista
+total = table[X - 1] * Y
+# Output formateado a dos decimales
+print(f"Total: R$ {total:.2f}")
