e890b954c20803461a36fbf506fac5399b49845e
Facundo <facundobarciabarate@gmail.com>
2024-09-17 02:57:54 -0300

    puedo adivinar la estructura de datos !

---
+++ I can guess the data structure!/challenge.py
@@ -0,0 +1,67 @@
+import sys
+from heapq import *
+
+def read_number():
+    '''
+        Lee el input para la cantidad de comandos que van a entrar
+    '''
+    
+    aux = sys.stdin.readline()
+    
+    if aux == '' or aux == '\n':
+        return None
+    
+    return int(aux)
+
+def find_type(n):
+    '''
+        Retorna el string del tipo de estructura de datos que logró descifrar
+    '''
+    
+    is_queue = is_stack = is_pqueue = True
+    pqueue_list = []
+    stack_list = []
+    queue_list = []
+    
+    cant = 0
+        
+    for I in range(n):
+        command_list = list(map(int, sys.stdin.readline().split()))
+        if command_list[0] == 1:
+            if is_queue:
+                queue_list.append(command_list[1])
+            if is_stack:
+                stack_list.append(command_list[1])
+            if is_pqueue:
+                heappush(pqueue_list, -command_list[1])
+            cant+= 1
+        else:
+            if cant == 0:
+                is_queue = is_stack = is_pqueue = False
+            else:
+                if is_queue and command_list[1] != queue_list.pop(0):
+                    is_queue = False
+                if is_stack and command_list[1] != stack_list.pop():
+                    is_stack = False
+                if is_pqueue and heappop(pqueue_list) != -command_list[1]:
+                    is_pqueue = False
+                cant -= 1
+    
+    if not is_queue and not is_stack and not is_pqueue:
+        return "impossible"
+    elif is_queue and not is_stack and not is_pqueue:
+        return "queue"
+    elif is_stack and not is_queue and not is_pqueue:
+        return "stack"
+    elif is_pqueue and not is_queue and not is_stack:
+        return "priority queue"
+    else:
+        return "not sure"
+
+while True:
+    n = read_number()
+    
+    if n is None:
+        break
+    
+    sys.stdout.write(find_type(n) + "\n")   
--- Jolly Jumpers/challenge.py
+++ Jolly Jumpers/challenge.py
@@ -16,6 +16,9 @@ def is_jolly(seq):
         es considerada un Jolly Jumper y "Not Jolly" en caso contrario
     '''
     
+    if len(seq) == 1:
+        return True
+    
     myset = set([abs(a-b) for a, b in zip(seq, seq[1:])]) #Creo un set por comprension. Cada posicion va a contener el valor abs del elemento i - i+1 de la lista de seq
     
     for I in range(1, len(seq)):
